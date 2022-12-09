5cbffc654dc587c7c77eedc505d209d5ea30629c
ThibaultGRAND <grand.thibault.info@gmail.com>
2022-12-09 12:59:39 +0100

    Ajout bar de vie + reglage des beug

--- new_year_2023.js
+++ new_year_2023.js
@@ -2,70 +2,75 @@ window.addEventListener('load',function(){
     let fenetreG = document.querySelector('.fenetres');
     let divPoints = document.querySelector('#divPoints');
     let lampe = fenetreG.querySelectorAll('rect');
-    let test = 0;
-    let tempBaisse = 5000;
-        let timer = setInterval(OnLampe, tempBaisse) ;
+    let lampeOn = fenetreG.querySelectorAll('rect.On');
+    let lampeOff = fenetreG.querySelectorAll('rect.off');
+    let test = lampeOn.length / lampe.length * 100;
+    console.log(lampeOn.length);
+    console.log(lampe.length);
+    let percentBar = document.querySelector('.lampLenght');
+
+    function PercentBar(){
+        test = lampeOn.length / lampe.length * 100;
+        percentBar.style.height = test +'%';
+    }
+
+    let tempBaisse = 700;
+
+        let timer = setInterval(OnLampe, tempBaisse);
+
     let pointsDisplay = document.querySelector('.points span');
     let points = 0;
     let time = divPoints.querySelector('.time span');
 
-    let lampeOn = fenetreG.querySelectorAll('rect.on');
-    let lampeOff = fenetreG.querySelectorAll('rect.off');
 
 
-    for(let i = 0; i < lampe.length; i++){
-        ClickLamp(lampe[i]);
+if(lampeOn.length > 0){
+    for(let i = 0; i < lampeOn.length; i++){
+        ClickLamp(lampeOn[i]);
     }
-
+}
         function refreshValues(){
      lampeOn = fenetreG.querySelectorAll('rect.on');
      lampeOff = fenetreG.querySelectorAll('rect.off');
     tempBaisse = tempBaisse - points * 10 ;
-    timer = setInterval(OnLampe, tempBaisse) ;
     }
 
     function ClickLamp(element){
         element.addEventListener("mouseenter", e => {
-            if(element.classList.contains('on')) {
-                element.classList.remove('on');
+            if(element.classList.contains('On')) {
+                element.classList.remove('On');
                 pointsDisplay.innerHTML = points ;
                 element.classList.add('off');
                 test = lampeOn.length / lampe.length;
-                console.log(test);
+                refreshValues() ;
+                console.log(lampeOff.length);
+                console.log(lampe.length);
 
+                if(lampeOff.length == lampe.length){
+                    console.log("fini");
+                    clearInterval(timer)
+                }
+                PercentBar();
             }
+
         })
-         refreshValues() ;
-    }
-    function OnLampe(){
-        let randomNombre = Math.floor(Math.random()) * lampeOff.length;
-
-        // if(time.innerHTML > 20){
-        //     console.log('ccc');
-        //     tempBaisse = time.innerHTML / points.innerHTML * 17;
-        // }
-        // console.log(time);
-        // console.log(points);
-        // console.log(tempBaisse);
-        // while(true){
-        //     if(lampe[randomNombre].classList.contains('On') && lampe.length !== lampeOn.length){
-        //         randomNombre = Math.floor(Math.random() * lampe.length);
-        //     }
-        //     else{
-        //         test = lampeOn.length / lampe.length;
-        //         break;
-        //     }
-        // }
-
-        let LampeAAllume = lampeOff[randomNombre];
-        console.log(Math.random() ) ;
-        LampeAAllume.classList.add("on");
-        LampeAAllume.classList.remove('off');
-
- refreshValues() ;
 
     }
+    function OnLampe() {
+    refreshValues() ;
+        if (lampeOff.length > 0) {
+            let randomNombre = Math.floor(Math.random() * lampeOff.length);
 
+            let LampeAEteindre = lampeOff[randomNombre];
+
+            LampeAEteindre.classList.add("On");
+            LampeAEteindre.classList.remove('off');
+            ClickLamp(LampeAEteindre);
+            refreshValues();
+
+        }
+        PercentBar();
+    }
     function addTime(){
         time.innerHTML++;
     }
@@ -92,14 +97,6 @@ window.addEventListener('load',function(){
 //
 // let personne ; // BOOL
 //
-//
-    let lightDiv = document.querySelector('.lightDiv')
-    for( let i = 1; i <= nombreLampe; i++){
-        let lampe = document.createElement('div')
-        lampe.classList.add('lampe')
-        lampe.classList.add('lampe'+i)
-        lightDiv.appendChild(lampe)
-    }
 //
 // function calculAffichagePoint(){
 //     // Ajoute les points et modifie l'affichage sur la vue
